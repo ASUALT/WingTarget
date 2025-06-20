3579650dbc6b2d6aad089b8e9960c6d0ea45e4df
ASUALT <qvantum.gamer@gmail.com>
2025-06-20 18:20:13 +0300

    Initial commit

---
+++ src/main/java/com/asualt/wingtarget/WingTarget.java
@@ -0,0 +1,15 @@
+package com.asualt.wingtarget;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WingTarget implements ModInitializer {
+	public static final String MOD_ID = "wingtarget";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/asualt/wingtarget/WingTargetClient.java
@@ -0,0 +1,10 @@
+package com.asualt.wingtarget;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WingTargetClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
